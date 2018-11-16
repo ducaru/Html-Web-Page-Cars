dae255bd6e99e48ce1b0f7f98b4d9495a953b1d8
Valentin Ducaru <valentin.ducaru@cerner.com>
2018-11-16 19:08:47 +0200

    [DEV] Implement column layout and inputs

--- controller/script.js
+++ controller/script.js
@@ -3,3 +3,20 @@ window.onload = function () {
         document.getElementById('modal').style.display = "none"
     };
 };
+
+function validate(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      alert("baaaa");
+    }
+  }
